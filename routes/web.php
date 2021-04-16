738f8573418eadabd8ddff5c7f2f545f7097ecaf
张焱 <1424588331@qq.com>
2021-04-16 10:53:23 +0800

    命名路由

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
  * get()传递了两个参数，第一个参数指明了URL 第二个参数指明了处理该 URL 的控制器动作
  * get 表明这个路由将会响应 GET 请求，并将请求映射到指定的控制器动作上
  */
-Route::get('/','StaticPagesController@home');
-Route::get('/help','StaticPagesController@help');
-Route::get('/about','StaticPagesController@about');
+Route::get('/','StaticPagesController@home')->name('home');
+Route::get('/help','StaticPagesController@help')->name('help');
+Route::get('/about','StaticPagesController@about')->name('about');
